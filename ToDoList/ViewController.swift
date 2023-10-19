915e5e0568de2ec61ea61ae2c4c862abedfbf0be
Gleb Klyga <klyga90@bk.ru>
2023-10-20 01:27:46 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Глеб Клыга on 20.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
